8d792513c5e91462dda1e76c017539b5aa16127a
Tyler Barabas <tylerbarabas@gmail.com>
2018-08-26 15:04:33 -0700

    heatin up in my pants

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './img/logo_white.png';
+import './img/ico_facebook.png';
+import './img/ico_twitter.png';
+import './img/ico_instagram.png';
+import './img/ico_pinterest.png';
 import './index.scss';
