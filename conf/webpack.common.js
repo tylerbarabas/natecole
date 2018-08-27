073f07dcc0b7b3a0ea09278e88e186f92ee9d650
Tyler Barabas <tylerbarabas@gmail.com>
2018-08-26 22:14:17 -0700

    remove eslint because fuck that shit

--- conf/webpack.common.js
+++ conf/webpack.common.js
@@ -32,11 +32,6 @@ module.exports = {
 
   module: {
     rules: [{
-      enforce: "pre", //to check source files, not modified by other loaders (like babel-loader)
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "eslint-loader"
-    }, {
       test: /\.js$/,
       exclude: /node_modules/,
       use: {
